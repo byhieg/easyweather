a9f98155a7547bd47f07823b7e948ddfbe552b32
shiqifeng <18581871304@163.com>
2016-09-18 15:14:05 +0800

    修改了更新天气的频率

--- app/src/main/java/com/weather/byhieg/easyweather/Tools/NetTool.java
+++ app/src/main/java/com/weather/byhieg/easyweather/Tools/NetTool.java
@@ -1,6 +1,7 @@
 package com.weather.byhieg.easyweather.Tools;
 
 import com.example.byhieglibrary.Net.HttpUtils;
+import com.example.byhieglibrary.Utils.LogUtils;
 import com.example.byhieglibrary.Utils.StringUtils;
 import com.google.gson.Gson;
 import com.weather.byhieg.easyweather.Bean.WeatherBean;
@@ -19,6 +20,8 @@ import okhttp3.Response;
  */
 public class NetTool {
 
+    private final static int UPDATERATE = 60;
+
     /**
      * 从网络获取order为1的城市的天气，并把天气放入数据库中
      *
@@ -31,18 +34,22 @@ public class NetTool {
         params.put("city", cityName);
         params.put("key", MyApplication.getHeweatherKey());
         String url = HttpUtils.url(MyApplication.getCityUrl(), null, params);
-        Response netResponse = HttpUtils.getAsyn(url);
-        String response = StringUtils.delPosOfString(netResponse.body().string(), pos);
-        WeatherBean weatherBean = gson.fromJson(response, WeatherBean.class);
+
         if(!HandleDaoData.isExistInCityWeather(cityName)){
+            Response netResponse = HttpUtils.getAsyn(url);
+            String response = StringUtils.delPosOfString(netResponse.body().string(), pos);
+            WeatherBean weatherBean = gson.fromJson(response, WeatherBean.class);
             HandleDaoData.insertCityWeather(PutDaoData.putWeatherData(weatherBean));
         }else{
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault());
-            Date date = simpleDateFormat.parse(MyJson.getWeather(weatherBean).getBasic().getUpdate().getLoc());
-            if((date.getTime() - (HandleDaoData.getCityWeather(cityName)).getUpdateTime().getTime()) > 1000 * 60 * 30){
+            Date nowDate = simpleDateFormat.parse(simpleDateFormat.format(new Date()));
+            if((nowDate.getTime() - (HandleDaoData.getCityWeather(cityName)).getUpdateTime().getTime()) > 1000 * 60 * UPDATERATE){
+                Response netResponse = HttpUtils.getAsyn(url);
+                LogUtils.e("NetTool","确实更新了天气");
+                String response = StringUtils.delPosOfString(netResponse.body().string(), pos);
+                WeatherBean weatherBean = gson.fromJson(response, WeatherBean.class);
                 HandleDaoData.updateCityWeather(PutDaoData.putWeatherData(weatherBean));
             }
-
         }
     }
 }
