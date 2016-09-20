98ce5800431352d36fa93b5f4c4c36c71b023ee9
shiqifeng <18581871304@163.com>
2016-09-20 17:00:45 +0800

    nothing

--- app/src/main/java/com/weather/byhieg/easyweather/Activity/MainActivity.java
+++ app/src/main/java/com/weather/byhieg/easyweather/Activity/MainActivity.java
@@ -148,7 +148,6 @@ public class MainActivity extends BaseActivity implements SwipeRefreshLayout.OnR
 
     private NetworkChangeReceiver networkChangeReceiver;
     private MyHandler handler = new MyHandler();
-    private TextView textView;
 
 
     @Override
@@ -200,7 +199,7 @@ public class MainActivity extends BaseActivity implements SwipeRefreshLayout.OnR
 
     @Override
     public void initView() {
-        textView = generateTextView();
+        generateTextView();
         toolbar.setTitle("成都");
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
@@ -521,7 +520,7 @@ public class MainActivity extends BaseActivity implements SwipeRefreshLayout.OnR
                 }).show();
     }
 
-    public TextView generateTextView(){
+    public void generateTextView(){
         TextView textView = new TextView(this);
         textView.setText("天气易变，注意天气变化");
         View[] view = {findViewById(R.id.toolbar),findViewById(R.id.view),findViewById(R.id.item_cloths),findViewById(R.id.item_sports)};
@@ -535,7 +534,6 @@ public class MainActivity extends BaseActivity implements SwipeRefreshLayout.OnR
         textView.setTextColor(ContextCompat.getColor(this,R.color.white));
         textView.setLayoutParams(lp);
         action_bar.addView(textView);
-        return textView;
 
     }
 
